0ed1722bd20c5da11c024da5fa318fad072a5bd8
Courtney Olivia <cowilliams1989@yahoo.com>
2018-04-25 01:55:58 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
+console.log("Keys loaded");
 
-exports.twitterKeys = {
+exports.twitter = {
   consumer_key: 'LY8xXH8x9IzUpzCeLa5Hzxk9R',
   consumer_secret: 'e3vZJmVC4rLL8ENtikAYpGXjacVwt7ISVzIaeBShTQlqN2uzVr',
   access_token_key: '962154849948356608-Qpbl45w11EZeXSBAvIBVkT6YhJZl2X1',
@@ -9,7 +9,7 @@ exports.twitterKeys = {
 
 
 
-exports.spotifyKeys = {
+exports.spotify = {
   id: 'fa11a3869f644353b52dbd9c2c4b4ef4',
   secret: '1f92c72fcd3a476396f528ee6fdc827e'
 };
