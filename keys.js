9cd4a9a17451180e12e803860eb5180e58e270fc
Courtney Olivia <cowilliams1989@yahoo.com>
2018-04-25 01:54:50 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,16 +1,16 @@
-console.log("Keys loaded");
+console.log('this is loaded');
 
-exports.twitter = {
-  consumer_key: process.env.LY8xXH8x9IzUpzCeLa5Hzxk9R,
-  consumer_secret: process.env.e3vZJmVC4rLL8ENtikAYpGXjacVwt7ISVzIaeBShTQlqN2uzVr,
-  access_token_key: process.env.962154849948356608-Qpbl45w11EZeXSBAvIBVkT6YhJZl2X1,
-  access_token_secret: process.env.yy2Q1aTA9yvwaER03KBY4UrCNe7wRdvOm2y9baiNZhF0O
+exports.twitterKeys = {
+  consumer_key: 'LY8xXH8x9IzUpzCeLa5Hzxk9R',
+  consumer_secret: 'e3vZJmVC4rLL8ENtikAYpGXjacVwt7ISVzIaeBShTQlqN2uzVr',
+  access_token_key: '962154849948356608-Qpbl45w11EZeXSBAvIBVkT6YhJZl2X1',
+  access_token_secret: 'yy2Q1aTA9yvwaER03KBY4UrCNe7wRdvOm2y9baiNZhF0O'
 };
 
 
 
-exports.spotify = {
-  id: process.env.fa11a3869f644353b52dbd9c2c4b4ef4,
-  secret: process.env.1f92c72fcd3a476396f528ee6fdc827e
+exports.spotifyKeys = {
+  id: 'fa11a3869f644353b52dbd9c2c4b4ef4',
+  secret: '1f92c72fcd3a476396f528ee6fdc827e'
 };
 
